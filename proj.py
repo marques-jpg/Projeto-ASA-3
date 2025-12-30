599bee33d759bc2580c162dcf3eb68419bd544ad
Guilherme Marques <gfelicianomarques@gmail.com>
2025-12-30 15:29:38 +0000

    commit1

---
+++ proj.py
@@ -0,0 +1,130 @@
+from pulp import *
+
+def main():
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+
+    iterator = iter(input_data)
+    try:
+        n_teams = int(next(iterator))
+        n_played = int(next(iterator))
+    except StopIteration:
+        return
+
+    current_points = [0] * n_teams
+
+    remaining_games = {}
+    for i in range(n_teams):
+        for j in range(i + 1, n_teams):
+            remaining_games[(i, j)] = 2
+
+    for _ in range(n_played):
+        try:
+            t1 = int(next(iterator)) - 1
+            t2 = int(next(iterator)) - 1
+            result = int(next(iterator))
+        except StopIteration:
+            break
+
+        pair = tuple(sorted((t1, t2)))
+        if pair in remaining_games:
+            remaining_games[pair] -= 1
+            if remaining_games[pair] < 0:
+                remaining_games[pair] = 0
+
+        if result == 0:
+            current_points[t1] += 1
+            current_points[t2] += 1
+        elif result == (t1 + 1):
+            current_points[t1] += 3
+        elif result == (t2 + 1):
+            current_points[t2] += 3
+
+    for target_team in range(n_teams):
+        target_remaining_matches = 0
+        target_matches_list = []
+
+        for i in range(n_teams):
+            if i == target_team:
+                continue
+            pair = tuple(sorted((target_team, i)))
+            count = remaining_games[pair]
+            target_remaining_matches += count
+            if count > 0:
+                target_matches_list.append((i, pair))
+
+        min_wins_found = -1
+
+        for w in range(target_remaining_matches + 1):
+            prob = LpProblem("Projeto3", LpMinimize)
+
+            vars_wins = {}
+            vars_draws = {}
+            vars_losses = {}
+
+            for (i, j), count in remaining_games.items():
+                if count > 0:
+                    x = LpVariable(f"x_{i}_{j}", 0, count, cat="Integer")
+                    y = LpVariable(f"y_{i}_{j}", 0, count, cat="Integer")
+                    z = LpVariable(f"z_{i}_{j}", 0, count, cat="Integer")
+
+                    vars_wins[(i, j)] = x
+                    vars_draws[(i, j)] = y
+                    vars_losses[(i, j)] = z
+
+
+
+                    prob += (x + y + z == count)
+
+            target_wins_vars = []
+            target_losses_vars = []
+
+            for opp, pair in target_matches_list:
+                (u, v) = pair
+                if target_team == u:
+                    target_wins_vars.append(vars_wins[pair])
+                    target_losses_vars.append(vars_losses[pair])
+                else:
+                    target_wins_vars.append(vars_losses[pair])
+                    target_losses_vars.append(vars_wins[pair])
+
+            if target_wins_vars:
+                prob += (lpSum(target_wins_vars) == w)
+            elif w > 0:
+                continue
+
+            if target_losses_vars:
+                prob += (lpSum(target_losses_vars) == 0)
+
+            points_target = current_points[target_team] + (3 * w) + (target_remaining_matches - w)
+
+            for opp in range(n_teams):
+                if opp == target_team:
+                    continue
+                future_points_opp = []
+
+                for (u, v), count in remaining_games.items():
+                    if count == 0:
+                        continue
+
+                    if u == opp:
+                        future_points_opp.append(3 * vars_wins[(u, v)])
+                        future_points_opp.append(vars_draws[(u, v)])
+                    elif v == opp:
+                        future_points_opp.append(3 * vars_losses[(u, v)])
+                        future_points_opp.append(vars_draws[(u, v)])
+
+                prob += (current_points[opp] + lpSum(future_points_opp) <= points_target)
+
+            status = prob.solve(GLPK_CMD(msg=0))
+
+            if status == LpStatusOptimal:
+                min_wins_found = w
+                break
+
+        print(min_wins_found)
+
+
+if __name__ == "__main__":
+    main()
